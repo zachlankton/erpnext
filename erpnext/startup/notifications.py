7bb958e62cf62e3e60f557bbb706db14d0eca857
zachlankton <zachlankton@gmail.com>
2016-04-04 13:14:56 -0400

    Update notifications.py

--- erpnext/startup/notifications.py
+++ erpnext/startup/notifications.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 def get_notification_config():
 	return { "for_doctype":
 		{
+			"Shift Run Summary": {"docstatus": 0},
 			"Issue": {"status": "Open"},
 			"Warranty Claim": {"status": "Open"},
 			"Task": {"status": "Overdue"},
